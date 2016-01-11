6e53757919c3637f88a2d4d26e5c4e63e85c9fb1
unknown <kafung1@697-334-Erebor.sf.k12.internal>
2016-01-11 11:31:35 -0900

    check code

--- build-tmp/source/GoogleBillboard.java
+++ build-tmp/source/GoogleBillboard.java
@@ -14,39 +14,35 @@ import java.io.IOException;
 
 public class GoogleBillboard extends PApplet {
 
+public double num;
 public final static String e = "2.718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059";  
 //public double num;
 public void setup()  
-{    
-
-    // String digits= e.substring(0,10);
-    // double dNum = Double.parseDouble(digits);
-     //System.out.println(dNum);   
-    // noLoop();  
+{
+	for(int a=0; a< e.length()-10; a++){
+			
+		String digits= e.substring(a, a+10);
+		double num = Double.parseDouble(digits);    
+		noLoop();
+	}
 }  
 public void draw()  
 {   
 //7427466391
-	
-	for(int a=0; a< e.length()-9; a++){
-			
 
-		String digits= e.substring(a, a+10);
-		double num = Double.parseDouble(digits);
-			if(isPrime(num)==true){
-			System.out.println(num);
-			break;
-	    		
-			}
-	} 
+	if(isPrime(num)==true){
+	System.out.println(num);
+	break;
+	}
+	
 }  
 public boolean isPrime(double dNum)  
 {   
 	
-	if(dNum%2==0||dNum%3==0||dNum%5==0||dNum%7==0||dNum%Math.sqrt(dNum)==0){
+	if(dNum==1||dNum%2==0||dNum%3==0||dNum%5==0||dNum%7==0||dNum%Math.sqrt(dNum)==0){
 		return false;
 	}
-	return true;
+		return true;
 } 
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "GoogleBillboard" };
